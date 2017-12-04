cf239a114bed93fa73bd4facb6fcaf9386ee99ad
Justin Wong <jusw85@hotmail.com>
2017-12-04 12:16:41 +0800

    Add stage character refresh on multi portrait

--- Assets/Project/Scripts/MultiPortraitCharacter.cs
+++ Assets/Project/Scripts/MultiPortraitCharacter.cs
@@ -5,16 +5,14 @@ using Fungus;
 public class MultiPortraitCharacter : Character {
 
     public List<PortraitEntry> portraitMap;
+    public Stage stage;
 
     [System.NonSerialized]
     public string key = "";
     private string prevFrameKey = "";
 
     private void Update() {
-        if (!Application.isPlaying) {
-            return;
-        }
-        if (prevFrameKey.Equals(key)) {
+        if (!Application.isPlaying || prevFrameKey.Equals(key)) {
             return;
         }
         prevFrameKey = key;
@@ -23,6 +21,21 @@ public class MultiPortraitCharacter : Character {
             if (entry.key.Equals(key)) {
                 portraits = entry.portraits;
                 break;
+            }
+        }
+
+        RefreshStageCharacter();
+    }
+
+    public void RefreshStageCharacter() {
+        if (stage == null) {
+            return;
+        }
+
+        if (State.onScreen) {
+            stage.ShowPortrait(this, State.portrait.name);
+            if (State.dimmed) {
+                State.portraitImage.color = stage.DimColor;
             }
         }
     }
